46d9a407f62f59c4ee7f7134757cf90855e06dcb
Shelby <shelbymollineaux@gmail.com>
2020-02-12 22:17:52 -0500

    adding in time + volume

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -32,12 +32,18 @@
     player.skipTo(event.target.value);
   });
 
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
+
+
   setInterval( () => {
   if (player.playState !== 'playing') { return; }
   const currentTime = player.getTime();
   const duration = player.getDuration();
   const percent = (currentTime / duration) * 100;
   $('#time-control .current-time').text( currentTime );
+  $('div.total-time').text(player.currentlyPlaying.duration);
   $('#time-control input').val(percent);
   }, 1000);
 
